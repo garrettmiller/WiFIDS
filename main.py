79d751840a39564a100b6c64b03a4b3324196b33
wifids <wifids@raspberrypi.(none)>
2015-02-09 20:45:44 +0000

    Code cleanup/commenting. -Garrett

--- main.py
+++ main.py
@@ -16,35 +16,35 @@ if os.geteuid() != 0:
 	exit("You need to have root privileges to run WiFIDS.\nPlease try again, but with 'sudo'. Exiting.")
 
 #Clean up any leftover running airmon-ng
-proc = subprocess.call(['airmon-ng', 'stop', 'mon0', 'mon1', 'mon2', 'mon3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cleanup = subprocess.call(['airmon-ng', 'stop', 'mon0', 'mon1', 'mon2', 'mon3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 #Put wlan0 into monitor mode.
-proc = subprocess.call(['airmon-ng', 'start', 'wlan0'], stdout=subprocess.PIPE)
+startmon = subprocess.call(['airmon-ng', 'start', 'wlan0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# THIS SECTION FOR LEARNING PURPOSES ONLY
+# THIS SECTION FOR LEARNING PURPOSES ONLY - BE SURE TO CHANGE
 # SOME EXAMPLE CODE BORROWED FROM http://pen-testing.sans.org/blog/2011/10/13/special-request-wireless-client-sniffing-with-scapy
 
 # Declare a Python list to keep track of client MAC addresses
-observedclients = []
+observedClients = []
 
 # The sniffmgmt() function is called each time Scapy receives a packet
 def sniffmgmt(p):
 	
-	# Define our tuple (an immutable list) of the 3 management frame
-	# subtypes sent exclusively by clients. I got this list from Wireshark.
-	stamgmtstypes = (0, 2, 4)
+	# Define our tuple of the 3 management frame
+	# subtypes sent exclusively by clients. From Wireshark.
+	managementFrameTypes = (0, 2, 4)
 
-	# Make sure the packet has the Scapy Dot11 layer present
+	# Make sure the packet is a WiFi packet
 	if p.haslayer(Dot11):
 
 		# Check to make sure this is a management frame (type=0) and that
-		# the subtype is one of our management frame subtypes indicating a
-		# wireless client
-		if p.type == 0 and p.subtype in stamgmtstypes:
+		# the subtype is one of our management frame subtypes
+		if p.type == 0 and p.subtype in managementFrameTypes:
 
 			# Check our list and if client isn't there, add to list.
-			if p.addr2 not in observedclients:
+			if p.addr2 not in observedClients:
 				print(p.addr2)
-				observedclients.append(p.addr2)
+				observedClients.append(p.addr2)
 
+#Actually run the sniffer. store=0 is required to keep memory from filling with packets.
 sniff(iface='mon0', prn=sniffmgmt, store=0)
