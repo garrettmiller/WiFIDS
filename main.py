6ab50c4443074c046f1447cc44000269bc3d1877
wifids <wifids@raspberrypi.(none)>
2015-02-11 00:09:49 +0000

    Enabled working authorized device lookup and config parsing. Is case insensitive, too. Updated config.cfg. -Garrett

--- main.py
+++ main.py
@@ -10,26 +10,31 @@ import os #Needed to check UID for root.
 import subprocess #Needed for system calls
 from scapy.all import * #Needed to do 802.11 stuff
 from netaddr import * #Needed for OUI lookup
+import ConfigParser #Needed to parse config file
+import string #Needed to work with strings
 
 #Check to see if we're root
 if os.geteuid() != 0:
 	exit("You need to have root privileges to run WiFIDS.\nPlease try again, but with 'sudo'. Exiting.")
 
-#Clean up any leftover running airmon-ng
+#Clean up any leftover running airmon-ng and put wlan0 into monitor mode.
 cleanup = subprocess.call(['airmon-ng', 'stop', 'mon0', 'mon1', 'mon2', 'mon3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-#Put wlan0 into monitor mode.
 startmon = subprocess.call(['airmon-ng', 'start', 'wlan0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# THIS SECTION FOR LEARNING PURPOSES ONLY - BE SURE TO CHANGE
-# SOME EXAMPLE CODE BORROWED FROM http://pen-testing.sans.org/blog/2011/10/13/special-request-wireless-client-sniffing-with-scapy
+# Read Configuration File and define lists
+config = ConfigParser.RawConfigParser()
+config.read('config.cfg')
+alertContacts = config.items("AlertContacts")
+authorizedClients = config.items("AuthorizedClients")
 
 # Declare a Python list to keep track of client MAC addresses
 observedClients = []
 
 # The sniffmgmt() function is called each time Scapy receives a packet
 def sniffmgmt(p):
-	
+	#Reinitialize "authorizedFlag"
+	authorizedFlag = 0
+
 	# Define our tuple of the 3 management frame
 	# subtypes sent exclusively by clients. From Wireshark.
 	managementFrameTypes = (0, 2, 4)
@@ -41,15 +46,32 @@ def sniffmgmt(p):
 		# the subtype is one of our management frame subtypes
 		if p.type == 0 and p.subtype in managementFrameTypes:
 
+			#Set MAC to what we received
+			mac = p.addr2
+
 			# Check our list and if client isn't there, add to list.
 			# Also perform OUI lookup on MAC. Checks for invalid OUI.
 			if p.addr2 not in observedClients:
 				try:
-					oui = EUI(p.addr2).oui.registration().org
+					oui = mac.oui.registration().org
 				except:
 					oui = "Invalid OUI"
 				print p.addr2 +  " -- " + oui
 				observedClients.append(p.addr2)
 
+				#Iterate through config file items to see if device is authorized.
+				for key, authorizedClient in authorizedClients:
+					if str(mac) == string.lower(authorizedClient):
+						authorizedFlag = 1
+						break
+					else:
+						authorizedFlag = 0
+					
+				#Perform appropriate action.
+				if authorizedFlag == 1:
+					print "Authorized Device - " + str(mac)
+				else:
+					print "Warning! Device " + str(mac) + " is unauthorized!"
+				
 #Actually run the sniffer. store=0 is required to keep memory from filling with packets.
 sniff(iface='mon0', prn=sniffmgmt, store=0)
